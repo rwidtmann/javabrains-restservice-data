26013aef816024420253b8f217fafc5477063cc2
richardwidtmann <kato1rjw>
2017-08-20 07:39:17 -0500

    Commit files Textbook and TopicServiceTest

---
+++ src/main/java/com/javabrains/restservice/topic/Textbook.java
@@ -0,0 +1,7 @@
+package com.javabrains.restservice.topic;
+
+/**
+ * Created by richardwidtmann on 8/20/17.
+ */
+public class Textbook {
+}
---
+++ src/test/java/com/javabrains/restservice/topic/TopicServiceTest.java
@@ -0,0 +1,11 @@
+package com.javabrains.restservice.topic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class TopicServiceTest {
+
+
+}
