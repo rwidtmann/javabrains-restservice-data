f708d170e4d11aa2b7f4075a669c83e2c8a4e6f6
richardwidtmann <kato1rjw>
2017-08-25 07:58:20 -0500

    Adding first html pages and file structure

---
+++ src/main/java/com/javabrains/restservice/topic/TopicViewController.java
@@ -0,0 +1,31 @@
+package com.javabrains.restservice.topic;
+
+
+import com.javabrains.restservice.topic.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class TopicViewController {
+
+
+    @GetMapping("/home")
+    public String index1() {
+        return "index";
+    }
+
+
+    @GetMapping("/topics-viewer")
+    public String index() {
+            return "topics-viewer/index";
+        }
+
+}
