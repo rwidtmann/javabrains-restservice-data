2eb6b2b683811273e423d607a0476cdf02daf893
richardwidtmann <kato1rjw>
2017-08-26 21:33:02 -0500

    Update Database migrations - make topic id primary auto increment and load homepage with existing topics for display

---
+++ out/production/javabrains-restservice-data/db/migration/V1_3__populateTopicTable.sql
@@ -0,0 +1,4 @@
+INSERT INTO topic(id, name, description) VALUES ("test1", "test1 framework", "test1 description");
+INSERT INTO topic(id, name, description) VALUES ("test2", "test2 framework", "test2 description");
+INSERT INTO topic(id, name, description) VALUES ("test3", "test3 framework", "test3 description");
+INSERT INTO topic(id, name, description) VALUES ("test4", "test4 framework", "test4 description");
